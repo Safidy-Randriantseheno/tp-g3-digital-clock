8973115366a71a4a034db6490782260b6dd3b8d2
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2022-06-27 12:23:38 +0200

    feat add App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import "./App.css";
-import Clock from './Clock';
-
-
-
+import { Clock, ClockClassComponent } from "./Clock/Clock";
+import Timer from "./setTimer/setTimer";
 function App() {
-  return <Clock></Clock>;
+  let [position, setPosition] = useState(0);
+  return ( <>
+      {
+        position == 0 ? <Clock /> : <Timer setState={setPosition} state={position} />
+      }
+      <Button state={position} setState={setPosition} />
+    </>
+  );
+}
+
+function Button({ setState, state }) {
+  let label = ["Set Timer", "Start", "Horloge"];
+  return (
+    <button className="btn" onClick={() => setState(state == 2 ? 0 : state + 1)}>{label[state]}</button>
+  );
 }
 
 export default App;
+
